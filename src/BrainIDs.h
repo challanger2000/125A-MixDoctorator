916cc70a64ba173f3506a8e5b85090d3168f6859
challanger2000 <challanger2000@gmx.de>
2026-09-23 21:29:11 +0200

    PoC: add coarse spectral analysis to src/BrainIDs.h

--- src/BrainIDs.h
+++ src/BrainIDs.h
@@ -1,8 +1,29 @@
 #pragma once
 #include "pluginterfaces/base/funknown.h"
 #include "pluginterfaces/vst/vsttypes.h"
+
 namespace MixDoctorator::Brain {
-static const Steinberg::FUID kProcessorUID(0xA125D011,0x10114A01,0xB0010011,0x125A0011);
-static const Steinberg::FUID kControllerUID(0xA125D012,0x10114A01,0xB0010011,0x125A0012);
-enum ParamID : Steinberg::Vst::ParamID { kDrumsConnected=200,kDrumsLevel,kBassConnected,kBassLevel,kGuitarConnected,kGuitarLevel };
-}
+
+static const Steinberg::FUID kProcessorUID(
+    0xA125D011,0x10114A01,0xB0010011,0x125A0011);
+
+static const Steinberg::FUID kControllerUID(
+    0xA125D012,0x10114A01,0xB0010011,0x125A0012);
+
+enum ParamID : Steinberg::Vst::ParamID {
+    kDrumsConnected=200,
+    kDrumsLevel,
+    kBassConnected,
+    kBassLevel,
+    kGuitarConnected,
+    kGuitarLevel,
+
+    kDrumsBassOverlap,
+    kDrumsBassBand,
+    kBassGuitarOverlap,
+    kBassGuitarBand,
+    kDrumsGuitarOverlap,
+    kDrumsGuitarBand
+};
+
+} // namespace MixDoctorator::Brain
