9df4a56aa26b5a6c531a6cfa38586e7321a115fe
challanger2000 <challanger2000@gmx.de>
2026-09-23 21:29:13 +0200

    PoC: add coarse spectral analysis to src/BrainProcessor.h

--- src/BrainProcessor.h
+++ src/BrainProcessor.h
@@ -1,20 +1,43 @@
 #pragma once
 #include "public.sdk/source/vst/vstaudioeffect.h"
 #include "MixDoctoratorIPC.h"
+
 namespace MixDoctorator::Brain {
+
 class Processor final : public Steinberg::Vst::AudioEffect {
 public:
     Processor();
-    static Steinberg::FUnknown* createInstance(void*){ return static_cast<Steinberg::Vst::IAudioProcessor*>(new Processor()); }
+
+    static Steinberg::FUnknown* createInstance(void*) {
+        return static_cast<Steinberg::Vst::IAudioProcessor*>(new Processor());
+    }
+
     Steinberg::tresult PLUGIN_API initialize(Steinberg::FUnknown*) override;
-    Steinberg::tresult PLUGIN_API setBusArrangements(Steinberg::Vst::SpeakerArrangement*,Steinberg::int32,
-        Steinberg::Vst::SpeakerArrangement*,Steinberg::int32) override;
+    Steinberg::tresult PLUGIN_API setBusArrangements(
+        Steinberg::Vst::SpeakerArrangement*, Steinberg::int32,
+        Steinberg::Vst::SpeakerArrangement*, Steinberg::int32) override;
     Steinberg::tresult PLUGIN_API canProcessSampleSize(Steinberg::int32) override;
     Steinberg::tresult PLUGIN_API setProcessing(Steinberg::TBool) override;
     Steinberg::tresult PLUGIN_API process(Steinberg::Vst::ProcessData&) override;
+
 private:
     IPC::SharedMemory ipc_;
-    double last_[6]{-1,-1,-1,-1,-1,-1};
-    void publishParam(Steinberg::Vst::ProcessData&,Steinberg::Vst::ParamID,double,int);
+    double last_[12]{
+        -1,-1,-1,-1,-1,-1,
+        -1,-1,-1,-1,-1,-1
+    };
+
+    void publishParam(
+        Steinberg::Vst::ProcessData&,
+        Steinberg::Vst::ParamID,
+        double,
+        int);
+
+    static void overlap(
+        const IPC::Snapshot& a,
+        const IPC::Snapshot& b,
+        double& score,
+        int& dominantBand) noexcept;
 };
-}
+
+} // namespace MixDoctorator::Brain
